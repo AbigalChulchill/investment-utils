236a235730be3a3436cba56ca80c4408ada1ecf2
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-08-18 18:12:57 +0300

    bugfix: PoloniexTrader occasionally fails to trade
    since we choose last ask/bid price first and then trade a limit order at that price, sometimes it happens that market goes so fast there is no ask/bid already after we checked it but before we traded it. The solution is 1) try 2nd best bid/ask
    2) retry the operation 3 times

--- poloniex_trader.py
+++ poloniex_trader.py
@@ -1,5 +1,6 @@
 from trader import Trader
 import poloniex_api, poloniex_config
+import time
 
 sym_to_pair={
     'bitcoin':          'USDT_BTC',
@@ -17,6 +18,8 @@ sym_to_pair={
     "ethereum-classic": "USDT_ETC",
 }
 
+MAX_RETRIES = 3
+
 class PoloniexTrader(Trader):
 
     @staticmethod
@@ -30,7 +33,35 @@ class PoloniexTrader(Trader):
 
 
     def buy_market(self, qty_usd: float) -> float:
-        market_price = float(self.api.returnOrderBook(self.pair)['asks'][0][0])
+        retries = MAX_RETRIES
+        while retries > 0:
+            try:
+                return self._buy_market(qty_usd)
+            except Exception:
+                if retries > 1:
+                    retries -= 1
+                    print("PoloniexTrader: buy: failed, retrying...")
+                    time.sleep(1)
+                else:
+                    raise
+
+
+    def sell_market(self, qty_tokens: float) -> float:
+        retries = MAX_RETRIES
+        while retries > 0:
+            try:
+                return self._sell_market(qty_tokens)
+            except Exception:
+                if retries > 1:
+                    retries -= 1
+                    print("PoloniexTrader: sell: failed, retrying...")
+                    time.sleep(1)
+                else:
+                    raise
+
+
+    def _buy_market(self, qty_usd: float) -> float:
+        market_price = float(self.api.returnOrderBook(self.pair)['asks'][1][0])
         response = self.api.buy(self.pair, market_price, qty_usd / market_price, {'fillOrKill': True})
         trades = response['resultingTrades']
         total_qty_coin = sum( [float(x['amount']) for x in trades] )
@@ -39,8 +70,8 @@ class PoloniexTrader(Trader):
         return [fill_price, total_qty_coin]
 
 
-    def sell_market(self, qty_tokens: float) -> float:
-        market_price = float(self.api.returnOrderBook(self.pair)['bids'][0][0])
+    def _sell_market(self, qty_tokens: float) -> float:
+        market_price = float(self.api.returnOrderBook(self.pair)['bids'][1][0])
         response = self.api.sell(self.pair, market_price, qty_tokens, {'fillOrKill': True})
         trades = response['resultingTrades']
         total_qty_coin = sum( [float(x['amount']) for x in trades] )
