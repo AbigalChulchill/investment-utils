f6034bc76718b512800c2aea89f171411955579c
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-09-18 02:44:11 +0300

    ftx_trader: simplified and refactored

--- lib/trader/ftx_trader.py
+++ lib/trader/ftx_trader.py
@@ -1,6 +1,10 @@
 import time
+
+from typing import Tuple
+
 from . import ftx_api
 from .trader import Trader
+
 
 sym_to_market={
     #crypto
@@ -33,38 +37,27 @@ class FtxTrader(Trader):
     def handles_sym(sym: str) -> bool:
         return sym in sym_to_market.keys()
 
-
     def __init__(self, sym: str, api_key: str, secret: str, subaccount: str):
-        self.market = sym_to_market[sym]
-        self.api = ftx_api.Ftx(api_key, secret, subaccount)
+        self._market = sym_to_market[sym]
+        self._api = ftx_api.Ftx(api_key, secret, subaccount)
 
-    def buy_market(self, qty_usd: float) -> float:
-        market_price = self.api.get_orderbook(self.market)['asks'][0][0]
-        order_id = self.api.place_order(market=self.market, side="buy", price=0, limit_or_market="market", size= qty_usd / market_price, ioc=False)
-        retries = 5
-        while retries > 0:
-            time.sleep(1)
-            response = self.api.get_order_status(order_id)
-            fill_qty = float(response['filledSize'])
-            req_qty = float(response['size'])
-            fill_price  = float(response['avgFillPrice'])
-            if (fill_qty - req_qty) < 0.001:
-                return [fill_price, fill_qty]
-            retries = retries - 1
-        raise Exception("not filled")
+    def buy_market(self, qty_usd: float) -> Tuple[float,float]:
+        market_price = self._api.get_orderbook(self._market)['asks'][0][0]
+        return self._finalize_order(self._api.place_order(market=self._market, side="buy", price=0, limit_or_market="market", size= qty_usd / market_price, ioc=False))
 
-    def sell_market(self, qty_tokens: float) -> float:
-        market_price = self.api.get_orderbook(self.market)['bids'][0][0]
-        order_id = self.api.place_order(market=self.market, side="sell", price=0, limit_or_market="market", size=qty_tokens, ioc=False)
-        retries = 5
-        while retries > 0:
-            time.sleep(1)
-            response = self.api.get_order_status(order_id)
-            fill_qty = float(response['filledSize'])
-            req_qty = float(response['size'])
-            fill_price  = float(response['avgFillPrice'])
-            if (fill_qty - req_qty) < 0.001:
-                return [fill_price, fill_qty]
-            retries = retries - 1
-        raise Exception("not filled")
+    def sell_market(self, qty_tokens: float) -> Tuple[float,float]:
+        return self._finalize_order(self._api.place_order(market=self._market, side="sell", price=0, limit_or_market="market", size=qty_tokens, ioc=False))
 
+
+    def _finalize_order(self, order_id: int) -> Tuple[float,float]:
+        fill_qty = 0
+        fill_price = 0
+        for _ in range(10):
+            time.sleep(0.5)
+            r = self._api.get_order_status(order_id)
+            if r['status'] == "closed":
+                fill_qty = float(r['filledSize'])
+                fill_price = float(r['avgFillPrice'])
+                break
+        return fill_price, fill_qty,
+        
--- lib/trader/trader.py
+++ lib/trader/trader.py
@@ -1,12 +1,14 @@
+from typing import Tuple
+
 class Trader(object):
 
-    def buy_market(self, qty_usd: float) -> float:
+    def buy_market(self, qty_usd: float) -> Tuple[float,float]:
         #
         # returns [average fill price, filled qty in tokens]
         #
         raise NotImplementedError()
 
-    def sell_market(self, qty_tokens: float) -> float:
+    def sell_market(self, qty_tokens: float) -> Tuple[float,float]:
         #
         # returns [average fill price, filled qty in tokens]
         #
