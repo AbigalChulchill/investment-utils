632e1ec10ae0b2c8f07ddade744959f282ecbdee
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-11-06 22:55:10 +0200

    no color for progressbar reverted, causes wrong CR if something printed while progressbar is active

--- lib/common/widgets.py
+++ lib/common/widgets.py
@@ -7,4 +7,7 @@ from rich.console import Console
 def track(*args: Any, **kwargs: Any):
     """a transient monochromatic progress bar
     """
-    return _rich_track(*args, **kwargs, transient=True, console=Console(no_color=True))
+    return _rich_track(*args, **kwargs,
+        transient=True,
+        #console=Console(no_color=True),
+        )
