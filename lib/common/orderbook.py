d047ffe137f4cb8c25adf39fc28058d6c71369bc
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-12-09 23:34:38 +0200

    fixed hiding of function parameter in a local var

--- lib/common/orderbook.py
+++ lib/common/orderbook.py
@@ -10,11 +10,10 @@ def estimate_fill_price(ob: List[Tuple[float,float]], qty: float) -> float:
         return ob[0][0]
     fill_remaining = qty
     orders: List[Tuple[float,float]] = []
-    for price,qty in ob:
-        this_fill = min(qty, fill_remaining)
-        orders.append((price, this_fill,))
+    for bucket_price,bucket_qty in ob:
+        this_fill = min(bucket_qty, fill_remaining)
+        orders.append((bucket_price, this_fill,))
         fill_remaining -= this_fill
         if isclose(fill_remaining, 0):
             break
-    avg_price = sum( [x[0]*x[1] for x in orders] ) / sum( [x[1] for x in orders] )
-    return avg_price
+    return sum( [x[0]*x[1] for x in orders] ) / sum( [x[1] for x in orders] )
