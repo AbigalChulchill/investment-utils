dc602ba30398da65ab7eb3b397bc243f83b71e10
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-12-21 23:21:44 +0200

    more ids

--- lib/common/id_map_okex.py
+++ lib/common/id_map_okex.py
@@ -1,8 +1,12 @@
 id_to_okex={
-    "decentraland":       "MANA-USDT",
-    "tezos":              "XTZ-USDT",
-    "hedera-hashgraph":   "HBAR-USDT",
-    "avalanche-2":        "AVAX-USDT",
     "algorand":           "ALGO-USDT",
+    "avalanche-2":        "AVAX-USDT",
+    "cosmos":             "ATOM-USDT",
+    "curve-dao-token":    "CRV-USDT",
+    "decentraland":       "MANA-USDT",
+    "hedera-hashgraph":   "HBAR-USDT",
     "illuvium":           "ILV-USDT",
+    "matic-network":      "MATIC-USDT",
+    "polkadot":           "DOT-USDT",
+    "tezos":              "XTZ-USDT",
 }
