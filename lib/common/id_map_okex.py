2c88377a6e2d24e187997d02189bc734318990b8
Oleksandr Dunayevskyy <oleksandr.dunayevskyy@gmail.com>
2021-12-23 10:47:14 +0200

    more ids

--- lib/common/id_map_okex.py
+++ lib/common/id_map_okex.py
@@ -1,6 +1,7 @@
 id_to_okex={
     "algorand":           "ALGO-USDT",
     "avalanche-2":        "AVAX-USDT",
+    "axie-infinity":      "AXS-USDT",
     "cosmos":             "ATOM-USDT",
     "curve-dao-token":    "CRV-USDT",
     "decentraland":       "MANA-USDT",
